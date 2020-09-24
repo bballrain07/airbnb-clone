a225ac1f8c1267eea07f4589d9d5346acb2c0ef6
bballrain07 <70263120+bballrain07@users.noreply.github.com>
2020-09-24 09:01:14 +0900

    Add files via upload
    This is only front-end airbnb project. 
    Not fully functional. 
    Enjoy! :)

---
+++ build/precache-manifest.c236c312bbb5365fd3aab9b6c8653592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d67715792777ae17984611586b8576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b5f4a91b9e76dda692",
+    "url": "/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "0bea1da33b358b43d375",
+    "url": "/static/css/main.35be8fb7.chunk.css"
+  },
+  {
+    "revision": "41b5f4a91b9e76dda692",
+    "url": "/static/js/2.72460c97.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.72460c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bea1da33b358b43d375",
+    "url": "/static/js/main.382b8f45.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
